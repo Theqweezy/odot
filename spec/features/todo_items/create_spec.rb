2145cb54caeec79f43def925e5b97b77930e2ab0
Jordan Gutman <jagutman87@gmail.com>
2015-01-27 12:00:30 -0500

    Added edit functions to todo_items and associated tests

--- spec/features/todo_items/create_spec.rb
+++ spec/features/todo_items/create_spec.rb
@@ -23,5 +23,27 @@ describe "adding todo items"  do
 		end
 	end
 
+	it "displays an error with no content" do
+		visit_todo_list(todo_list)
+		click_link "New Todo Item"
+		fill_in "Content", with: ""
+		click_button "Save"
+		within("div.flash") do
+			expect(page).to have_content("There was an error saving this todo item")
+		end
+		expect(page).to have_content("Content can't be blank")
+	end
+
+	it "displays an error with less than 2 characters" do
+		visit_todo_list(todo_list)
+		click_link "New Todo Item"
+		fill_in "Content", with: "1"
+		click_button "Save"
+		within("div.flash") do
+			expect(page).to have_content("There was an error saving this todo item")
+		end
+		expect(page).to have_content("Content is too short")
+	end
+
 
 end
