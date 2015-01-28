622dbdf41571e5c00529eab58b64de30eb1c581c
Jordan Gutman <jagutman87@gmail.com>
2015-01-28 14:19:24 -0500

    extracted todo list helper

--- spec/features/todo_items/create_spec.rb
+++ spec/features/todo_items/create_spec.rb
@@ -4,12 +4,7 @@ require 'spec_helper'
 describe "adding todo items"  do
 	let!(:todo_list) { TodoList.create(title: "Grocery list", description: "Groceries") }
 	
-	def visit_todo_list(list)
-		visit "/todo_lists"
-		within "#todo_list_#{list.id}" do
-			click_link("List Items")
-		end
-	end
+	
 
 
 	it "is successful with valid content" do
